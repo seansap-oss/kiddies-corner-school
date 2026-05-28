c0e3d4f828d3288a27f0913cc751dfd12600df51
seansap-oss <seansap@gmail.com>
2026-05-28 14:36:34 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontFamily: "Arial",
+      background: "#f5f5f5"
+    }}>
+      <h1>Kiddies Corner School Website</h1>
+    </div>
+  );
+}
